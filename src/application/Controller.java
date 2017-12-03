36b8e52d7cdd0f2a0e0a8c696e62e0074c0b1777
Ruben <escribanomartinruben@gmail.com>
2017-12-03 12:50:27 +0100

    Sunday Final Commit

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
@@ -16,15 +18,23 @@ public class Controller implements Initializable {
 
 	@FXML
 	private AnchorPane pane1;
+	@FXML
+	private ImageView image1;
 
 	@FXML
 	private AnchorPane pane2;
+	@FXML
+	private ImageView image2;
 
 	@FXML
 	private AnchorPane pane3;
+	@FXML
+	private ImageView image3;
 
 	@FXML
 	private AnchorPane pane4;
+	@FXML
+	private ImageView image4;
 
 	@FXML
 	private Label countLabel;
@@ -50,20 +60,20 @@ public class Controller implements Initializable {
 	void nextAction(ActionEvent event) {
 
 		if (showSlide == 0) {
-			translateAnimation(0.5, pane2, -615);
+			translateAnimation(0.7, pane2, -615);
 			showSlide++; // showSlide=1
-			countLabel.setText("2/4");
+			countLabel.setText(" STEP 2/4");
 		} else if (showSlide == 1) {
 
-			translateAnimation(0.5, pane3, -615);
+			translateAnimation(0.7, pane3, -615);
 			showSlide++; // showSlide=2
-			countLabel.setText("3/4");
+			countLabel.setText("STEP 3/4");
 
 		} else if (showSlide == 2) {
 
-			translateAnimation(0.5, pane4, -615);
+			translateAnimation(0.7, pane4, -615);
 			showSlide++; // showSlide=3
-			countLabel.setText("4/4");
+			countLabel.setText("STEP 4/4");
 
 		} else {
 			System.out.println("No more slides");
@@ -93,5 +103,23 @@ public class Controller implements Initializable {
 			countLabel.setText("3/4");
 
 		}
+	}
+	
+	@FXML
+	public void imageBlurON (){
+		image1.setEffect(new GaussianBlur(7));
+		image2.setEffect(new GaussianBlur(7));
+		image3.setEffect(new GaussianBlur(7));
+		image4.setEffect(new GaussianBlur(7));
+		
+	}
+	
+	@FXML
+	public void BlurOFF (){
+		image1.setEffect(new GaussianBlur(0));
+		image2.setEffect(new GaussianBlur(0));
+		image3.setEffect(new GaussianBlur(0));
+		image4.setEffect(new GaussianBlur(0));
+		
 	}
 }
